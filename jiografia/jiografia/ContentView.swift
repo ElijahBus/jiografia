ab32e65f32ce285d7aeb1765c7841e422a875811
cedricbahirwe <cedricaganzebahirwe@gmail.com>
2021-02-23 21:08:09 +0200

    Update vstack to grid

--- jiografia/jiografia/ContentView.swift
+++ jiografia/jiografia/ContentView.swift
@@ -9,13 +9,74 @@ import SwiftUI
 
 struct ContentView: View {
     @State var countries = DataLoader().countries
+    @State var correctAnswer = 0
+    @State var answeredQuestions = 0
+    @State var score = 0
+    @State var showScoreAlert = false
+    @State var navigationTitle: String = ""
+    @State var alertTitle: String = ""
+    @State var alertMessage: String = ""
+    
+    
     var body: some View {
-        NavigationView {
-            List(countries, id: \.code) { country in
-                Text(country.name)
+        VStack {
+            VStack(spacing: 5) {
+                Text("Your score is: \(score)")
+                Text(navigationTitle)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.8)
+                
             }
-            .navigationTitle("\(countries.count) Countries")
+            .padding(.horizontal, 5)
+            .font(.system(size: 20, weight: .bold, design: .rounded))
+            .foregroundColor(Color(.systemBackground))
+            .frame(maxWidth: .infinity)
+            .padding(.bottom, 10)
+            Spacer()
+            GridStack(rows: 2, columns: 2) { (row, column) in
+                let index = indexAt(row, column)
+                Button(action: {
+                    buttonTapped(index)
+                }) {
+                    Text("\(countries[index].emoji)")
+                }
+                .font(.system(size: 160))
+                .minimumScaleFactor(0.1)
+            }
+            Spacer()
+            
         }
+        .background(Color(.label).edgesIgnoringSafeArea(.vertical))
+        .alert(isPresented: $showScoreAlert) {
+            Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Continue"), action: askQuestion))
+        }
+        .onAppear(perform: askQuestion)
+    }
+    
+    private func indexAt(_ row: Int, _ column: Int) -> Int {
+        return  row * 2 + column
+    }
+    
+    private func buttonTapped(_ index: Int) {
+        answeredQuestions += 1
+        if index == correctAnswer {
+            alertTitle = "Correct"
+            score += 10
+            alertMessage = "Your score is \(score)."
+        } else {
+            alertTitle = "Wrong"
+            score -= 10
+            alertMessage = "Wrong! That’s the flag of \(countries[index].name)."
+        }
+        let isfinal = answeredQuestions % 10 == 0
+        alertMessage = isfinal ? "Your final score is \(score)." : alertMessage
+        showScoreAlert.toggle()
+    }
+    
+    private func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+        navigationTitle =  "Find: " + countries[correctAnswer].name.uppercased()
     }
 }
 
@@ -24,3 +85,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
